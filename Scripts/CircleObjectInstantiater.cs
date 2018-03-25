5b43f0bff265eccf9742b8b8c78a9fe717152079
rarudo <rarudonet@gmail.com>
2018-03-25 16:11:34 +0900

    Refactor: オブジェクトの名前を変更

---
+++ Scripts/CircleObjectInstantiater.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleObjectInstantiater : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject _object; 
+	[SerializeField]
+	private float _radius;
+	[SerializeField]
+	private int _quantity;
+	//回転する量
+	private float _rotateDiff;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_rotateDiff = 360 / _quantity;
+		InstantiateObject();
+	}
+
+	void InstantiateObject()
+	{
+		GameObject[] objects = new GameObject[_quantity];
+		for (int i = 0; i < _quantity; i++)
+		{
+			var obj = Instantiate(_object);
+			obj.transform.position = transform.position;
+			obj.transform.rotation = transform.rotation;
+			obj.transform.position += obj.transform.right * _radius;
+			transform.Rotate(0,_rotateDiff,0);
+			obj.transform.Rotate(-90,0,0);
+			objects[i] = obj;
+		}
+
+		foreach (var obj in objects)
+		{
+			obj.transform.parent = transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
